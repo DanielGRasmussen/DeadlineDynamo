0008a5e23f74b7e1c5360990a7c3e543326cf446
DanielGRasmussen <104102130+DanielGRasmussen@users.noreply.github.com>
2024-04-09 10:29:11 -0700

    Added zip command
    Added ability to zip files to upload to chrome webstore.

---
+++ zip.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Folders to zip
+folders="dist images third_party"
+
+# Files to zip (modify paths if needed)
+files="manifest.json privacy-policy.md README.md welcome.html"
+
+# ZIP filename
+zip_name="Deadline Dynamo - Chrome Minimized.zip"
+
+# Create the ZIP file
+zip -r "$zip_name" $folders $files
