1cc1c7c3375678d775d4a3c3f904a35ba3c05a0a
DanielGRasmussen <104102130+DanielGRasmussen@users.noreply.github.com>
2024-04-10 16:13:23 -0700

    Fixed zip.sh. For real this time.

--- zip.sh
+++ zip.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-zip -r "Deadline Dynamo - Firefox Full.zip" . -x ".git/*" -x ".idea/*" -x "node_modules/*" -x "dist/*"
+zip -r "Deadline Dynamo - Firefox Full" . -x ".git/*" -x ".idea/*" -x "node_modules/*" -x "dist/*"
 
 # Folders to zip
 folders="dist images third_party"
@@ -9,7 +9,7 @@ folders="dist images third_party"
 files="manifest.json privacy-policy.md README.md welcome.html"
 
 # ZIP filename
-zip_name="Deadline Dynamo - Firefox Minimized.zip"
+zip_name="Deadline Dynamo - Firefox Minimized"
 
 # Create the ZIP file
 zip -r "$zip_name" $folders $files
